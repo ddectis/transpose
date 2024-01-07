5b1d0b24e36519c880df0259ab86c039dc579b9c
Dan Dectis <dandectis@gmail.com>
2024-01-07 09:02:21 -0800

    design improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${mono.className}`}>
-        
+
 
         {!isSongSelected ? (
           <div>
